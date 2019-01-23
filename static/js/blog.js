4379f3fb51df01156c645176efebeda48a105d40
xiaoheixian <1434331095@qq.com>
2019-01-23 15:43:56 +0800

    添加留言

--- static/js/blog.js
+++ static/js/blog.js
@@ -6,14 +6,14 @@
  @Site：http://www.layui.com/template/xianyan/
     
  */
-layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
+layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (exports) {
   var element = layui.element
-  ,form = layui.form
-  ,laypage = layui.laypage
-  ,$ = layui.jquery
-  ,laytpl = layui.laytpl;
-  
-  //xiaoheixian
+    , form = layui.form
+    , laypage = layui.laypage
+    , $ = layui.jquery
+    , laytpl = layui.laytpl;
+
+  //by xiaoheixian
   $(function () {
     var code = getParam("code");
     if (code != null) {
@@ -33,7 +33,7 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
           var token = new Array(); //定义一数组
           token = strs[0].split("="); //字符分割
           var access_token = token[1];
-          window.location.href='https://xiaoheixian.github.io/message?t='+access_token;
+          window.location.href = 'https://xiaoheixian.github.io/message?t=' + access_token;
         }
       });
     }
@@ -41,17 +41,18 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
     if (access_token != null) {
       $.ajax({
         type: "GET",
-        url: "https://api.github.com/user?access_token="+access_token,
+        url: "https://api.github.com/user?access_token=" + access_token,
         contentType: "application/json",
         success: function (data) {
           localStorage.setItem("name", data.name);
           localStorage.setItem("avatar_url", data.avatar_url);
+          localStorage.setItem("html_url", data.html_url);
         }
       });
     }
   });
   /** 
-   * xiaoheixian
+   * by xiaoheixian
    * 获取指定的URL参数值 
    * URL:http://www.quwan.com/index?name=tyler 
    * 参数：paramName URL参数 
@@ -68,20 +69,20 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
   }
 
   //statr 分页
-  
+
   laypage.render({
     elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
-    ,count: 50 //数据总数，从服务端得到
-    ,theme: '#1e9fff'
+    , count: 50 //数据总数，从服务端得到
+    , theme: '#1e9fff'
   });
-  
+
   // end 分頁
- 
+
 
 
   // start 导航显示隐藏
-  
-  $("#mobile-nav").on('click', function(){
+
+  $("#mobile-nav").on('click', function () {
     $("#pop-nav").toggle();
   });
 
@@ -91,57 +92,57 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
 
 
   //start 评论的特效
-  
+
   (function ($) {
     $.extend({
-        tipsBox: function (options) {
-          options = $.extend({
-            obj: null,  //jq对象，要在那个html标签上显示
-            str: "+1",  //字符串，要显示的内容;也可以传一段html，如: "<b style='font-family:Microsoft YaHei;'>+1</b>"
-            startSize: "12px",  //动画开始的文字大小
-            endSize: "30px",    //动画结束的文字大小
-            interval: 600,  //动画时间间隔
-            color: "red",    //文字颜色
-            callback: function () { }    //回调函数
-          }, options);
+      tipsBox: function (options) {
+        options = $.extend({
+          obj: null,  //jq对象，要在那个html标签上显示
+          str: "+1",  //字符串，要显示的内容;也可以传一段html，如: "<b style='font-family:Microsoft YaHei;'>+1</b>"
+          startSize: "12px",  //动画开始的文字大小
+          endSize: "30px",    //动画结束的文字大小
+          interval: 600,  //动画时间间隔
+          color: "red",    //文字颜色
+          callback: function () { }    //回调函数
+        }, options);
 
-          $("body").append("<span class='num'>" + options.str + "</span>");
+        $("body").append("<span class='num'>" + options.str + "</span>");
 
-          var box = $(".num");
-          var left = options.obj.offset().left + options.obj.width() / 2;
-          var top = options.obj.offset().top - 10;
-          box.css({
-            "position": "absolute",
-            "left": left + "px",
-            "top": top + "px",
-            "z-index": 9999,
-            "font-size": options.startSize,
-            "line-height": options.endSize,
-            "color": options.color
-          });
-          box.animate({
-            "font-size": options.endSize,
-            "opacity": "0",
-            "top": top - parseInt(options.endSize) + "px"
-          }, options.interval, function () {
-            box.remove();
-            options.callback();
-          });
-        }
-      });
-  })($); 
+        var box = $(".num");
+        var left = options.obj.offset().left + options.obj.width() / 2;
+        var top = options.obj.offset().top - 10;
+        box.css({
+          "position": "absolute",
+          "left": left + "px",
+          "top": top + "px",
+          "z-index": 9999,
+          "font-size": options.startSize,
+          "line-height": options.endSize,
+          "color": options.color
+        });
+        box.animate({
+          "font-size": options.endSize,
+          "opacity": "0",
+          "top": top - parseInt(options.endSize) + "px"
+        }, options.interval, function () {
+          box.remove();
+          options.callback();
+        });
+      }
+    });
+  })($);
 
-  function niceIn(prop){
+  function niceIn(prop) {
     prop.find('i').addClass('niceIn');
-    setTimeout(function(){
-      prop.find('i').removeClass('niceIn'); 
-    },1000);    
+    setTimeout(function () {
+      prop.find('i').removeClass('niceIn');
+    }, 1000);
   }
 
   $(function () {
-    $(".like").on('click',function () {
-     
-      if(!($(this).hasClass("layblog-this"))){
+    $(".like").on('click', function () {
+
+      if (!($(this).hasClass("layblog-this"))) {
         this.text = '已赞';
         $(this).addClass('layblog-this');
         $.tipsBox({
@@ -153,9 +154,9 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
         niceIn($(this));
         layer.msg('点赞成功', {
           icon: 6
-          ,time: 1000
+          , time: 1000
         })
-      } 
+      }
     });
   });
 
@@ -163,7 +164,7 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
 
 
   // start点赞图标变身
-  $('#LAY-msg-box').on('click', '.info-img', function(){
+  $('#LAY-msg-box').on('click', '.info-img', function () {
     $(this).addClass('layblog-this');
   })
 
@@ -171,71 +172,80 @@ layui.define(['element', 'form','laypage','jquery','laytpl'],function(exports){
   // end点赞图标变身
 
   //end 提交
-  $('#item-btn').on('click', function(){
+  $('#item-btn').on('click', function () {
     var elemCont = $('#LAY-msg-content')
-    ,content = elemCont.val();
+      , content = elemCont.val();
 
-    console.log(localStorage.getItem("name"));
-    if(localStorage.getItem("name") == "" || localStorage.getItem("name") == null){
+    //by xiaoheixian
+    if (localStorage.getItem("name") == "" || localStorage.getItem("name") == null) {
 
       layer.open({
         type: 1
-        ,offset: 'auto' //具体配置参考：http://www.layui.com/doc/modules/layer.html#offset
-        ,id: 'layerDemo'+'auto' //防止重复弹出
-        ,content: '<div style="padding: 20px 100px;">'+ '请先登录' +'</div>'
-        ,btn: 'github登录'
-        ,btnAlign: 'c' //按钮居中
-        ,shade: 0 //不显示遮罩
-        ,yes: function(){
-          window.location.href='https://github.com/login/oauth/authorize?client_id=5315dcce3ae5dc901d87&redirect_uri=https://xiaoheixian.github.io/message';
+        , offset: 'auto' //具体配置参考：http://www.layui.com/doc/modules/layer.html#offset
+        , id: 'layerDemo' + 'auto' //防止重复弹出
+        , content: '<div style="padding: 20px 100px;">' + '请先登录' + '</div>'
+        , btn: 'github登录'
+        , btnAlign: 'c' //按钮居中
+        , shade: 0 //不显示遮罩
+        , yes: function () {
+          //by xiaoheixian
+          window.location.href = 'https://github.com/login/oauth/authorize?client_id=5315dcce3ae5dc901d87&redirect_uri=https://xiaoheixian.github.io/message';
         }
       });
       return elemCont.focus();
     }
 
-    if(content.replace(/\s/g, '') == ""){
+    if (content.replace(/\s/g, '') == "") {
       layer.msg('请先输入留言');
       return elemCont.focus();
     }
 
     var view = $('#LAY-msg-tpl').html()
 
-    //模拟数据
-    // ,data = {
-    //   username: '闲心'
-    //   ,avatar: '../res/static/images/info-img.png'
-    //   ,praise: 0
-    //   ,content: content
-    // };
-    ,data = {
+      //模拟数据 by xiaoheixian
+      , data = {
         username: localStorage.getItem("name")
-        ,avatar: localStorage.getItem("avatar_url")
-        ,praise: 0
-        ,content: content
-    };
+        , avatar: localStorage.getItem("avatar_url")
+        , praise: 0
+        , content: content
+        , html_url: localStorage.getItem("html_url")
+      };
 
     //模板渲染
-    laytpl(view).render(data, function(html){
-      $('#LAY-msg-box').prepend(html);
-      elemCont.val('');
-      layer.msg('留言成功', {
-        icon: 1
-      })
+    laytpl(view).render(data, function (html) {
+      $.ajax({
+        url: "http://cloud.bmob.cn/7d0718562ae91957/addMessage",
+        dataType: 'jsonp',
+        data: JSON.stringify({
+          "name":data.name,
+          "avatar_url":data.avatar,
+          "html_url":data.html_url,
+          "message_content":data.content
+        }),
+        success: function (result) {
+          $('#LAY-msg-box').prepend(html);
+          elemCont.val('');
+          layer.msg('留言成功', {
+            icon: 1
+          })
+        }
+      });
+
     });
 
   })
 
   // start  图片遮罩
   var layerphotos = document.getElementsByClassName('layer-photos-demo');
-  for(var i = 1;i <= layerphotos.length;i++){
+  for (var i = 1; i <= layerphotos.length; i++) {
     layer.photos({
-      photos: ".layer-photos-demo"+i+""
-      ,anim: 0
-    }); 
+      photos: ".layer-photos-demo" + i + ""
+      , anim: 0
+    });
   }
   // end 图片遮罩
 
 
   //输出test接口
-  exports('blog', {}); 
+  exports('blog', {});
 });  
