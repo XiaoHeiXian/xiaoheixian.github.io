654d275e27ca8c2225dabb817574c58bd61f4a24
xianshengnan <1434331095@qq.com>
2019-05-24 15:05:55 +0800

    修改后台地址

--- static/js/blog.js
+++ static/js/blog.js
@@ -205,13 +205,12 @@ layui.define(['element', 'form', 'laypage', 'jquery', 'laytpl'], function (expor
     //模板渲染
     laytpl(view).render(data, function (html) {
       $.ajax({
-        url: "http://cloud.bmob.cn/7d0718562ae91957/addMessage"
+        url: "http://www.wannengde.cn:8080/blog/message/addMessage"
             +"?name="+data.username
             +"&avatar_url="+data.avatar
             +"&html_url="+data.html_url
             +"&message_content="+data.content,
-        dataType: 'jsonp',
-        jsonp: 'callback',
+        dataType: 'json',
         success: function (result) {
           $('#LAY-msg-box').prepend(html);
           elemCont.val('');
