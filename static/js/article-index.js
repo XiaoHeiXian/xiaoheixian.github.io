cc7e504161c0ecb0c4fd9856dd32c48de02ae8f4
XSN <1434331095@qq.com>
2026-09-04 01:51:12 +0800

    docs: delete 2026-09-02-redis

--- static/js/article-index.js
+++ static/js/article-index.js
@@ -1,15 +1,5 @@
 window.BLOG_ARTICLES = [
-  {
-    "id": "2026-09-02-redis",
-    "title": "redis",
-    "url": "posts/2026-09-02-redis.html",
-    "publishedAt": "2026-09-02",
-    "category": "redis",
-    "tags": [
-      "redis"
-    ],
-    "summary": "redis"
-  },
+  
   {
     "id": "2026-09-02-test2",
     "title": "测试2",
